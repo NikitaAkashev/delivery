36a96757b2c822dd6eb52a8a0abdcca42fab7605
Akashev Nikita <regspambox@yandex.ru>
2015-08-23 23:49:22 +0500

    При заполнении цен можно указывать признак отображения строки

--- com_calculator/admin/fuck_joomla/price_filler.php
+++ com_calculator/admin/fuck_joomla/price_filler.php
@@ -31,8 +31,8 @@ TXT;
 			$r = array_map("FormatValue", $data);
 			
 			$query .= <<<TXT
-INSERT INTO {$prefix}_delivery_rate (`city_from`, `city_to`, `zone`, `tariff`, `provider`, `min_days`, `max_days`, `delivery_hours`)
-values ({$r[0]}, {$r[1]}, {$r[2]}, {$r[3]}, {$r[4]}, {$r[5]}, {$r[6]}, {$r[7]});
+INSERT INTO {$prefix}_delivery_rate (`city_from`, `city_to`, `zone`, `tariff`, `provider`, `min_days`, `max_days`, `delivery_hours`, `is_enabled`)
+values ({$r[0]}, {$r[1]}, {$r[2]}, {$r[3]}, {$r[4]}, {$r[5]}, {$r[6]}, {$r[7]}, {$r[20]});
 
 INSERT INTO {$prefix}_delivery_weight_price (`rate`,`from`,`to`,`base_price`,`overweight_cost`)
 SELECT MAX(`rate`), {$r[8]}, {$r[9]}, {$r[10]}, {$r[11]}
