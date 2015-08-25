1634ee4a7468a9dfe19057d1760497d49ce433af
Akashev Nikita <regspambox@yandex.ru>
2015-08-25 21:30:57 +0500

    Исправление строки

--- com_calculator/admin/fuck_joomla/price_filler.php
+++ com_calculator/admin/fuck_joomla/price_filler.php
@@ -33,7 +33,7 @@ TXT;
 			
 			$query .= <<<TXT
 INSERT INTO {$prefix}_delivery_rate (`city_from`, `city_to`, `zone`, `tariff`, `provider`, `min_days`, `max_days`, `delivery_hours`, `is_enabled`)
-values ({$r[0]}, {$r[1]}, {$r[2]}, {$r[3]}, {$r[4]}, {$r[5]}, {$r[6]}, '{$r[7]}', {$r[20]});
+values ({$r[0]}, {$r[1]}, {$r[2]}, {$r[3]}, {$r[4]}, {$r[5]}, {$r[6]}, {$r[7]}, {$r[20]});
 
 INSERT INTO {$prefix}_delivery_weight_price (`rate`,`from`,`to`,`base_price`,`overweight_cost`)
 SELECT MAX(`rate`), {$r[8]}, {$r[9]}, {$r[10]}, {$r[11]}
