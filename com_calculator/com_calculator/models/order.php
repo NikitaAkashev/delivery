a2c3574857c509baa31b28f3dda2cdececb251c6
Akashev Nikita <regspambox@yandex.ru>
2015-09-15 23:32:02 +0500

    Изменение скидки
    Скидка применяется к окончательной цене клиента

--- com_calculator/com_calculator/models/order.php
+++ com_calculator/com_calculator/models/order.php
@@ -275,11 +275,11 @@ from(
 				// вычислим оценочную стоимость
 				$assessed_value_price = $this->assessed_value == 0 ? 0 : max($rate->avp_base_price + $rate->overprice_percent * (ceil($this->assessed_value) - $rate->avp_bottom), $rate->min_assessed_price);
 				
-				// вычислим внутреннюю цену (НДС, если цена без него * вес * скидка * коэф. городов + оценочная стоимость)
-				$this->prices[$i]->inner_price = round(($rate->prices_with_nds == 0 ? 1 + $this->nds : 1) * $weight_price * (1 - $rate->discount_factor) * ($rate->cf_factor_inner + $rate->ct_factor_inner - 1) + $assessed_value_price, 2);
+				// вычислим внутреннюю цену (НДС, если цена без него * вес * коэф. городов + оценочная стоимость)
+				$this->prices[$i]->inner_price = round(($rate->prices_with_nds == 0 ? 1 + $this->nds : 1) * $weight_price * ($rate->cf_factor_inner + $rate->ct_factor_inner - 1) + $assessed_value_price, 2);
 				
-				// цена клиента = внутренняя * наценку * доп. коэфициент городов
-				$this->prices[$i]->customer_price = $this->RoundPrice($this->prices[$i]->inner_price * $rate->margin * ($rate->cf_factor_outer + $rate->ct_factor_outer - 1)) + $courier_price;
+				// цена клиента = скидка * хитро_округленное(внутренняя * наценку * доп. коэфициент городов + цена доставки)
+				$this->prices[$i]->customer_price = round((1 - $rate->discount_factor) * ($this->RoundPrice($this->prices[$i]->inner_price * $rate->margin * ($rate->cf_factor_outer + $rate->ct_factor_outer - 1)) + $courier_price), 2);
 				
 				// прибыль
 				$this->prices[$i]->profit = $this->prices[$i]->customer_price - $this->prices[$i]->inner_price;
