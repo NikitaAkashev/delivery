17845a34b12633bb776eb91914f0e040baf818b5
Akashev Nikita <regspambox@yandex.ru>
2016-01-28 20:37:43 +0500

    Небольшие исправления
    Теперь используется placeholder
    Изменен экшен у формы, теперь ведет на baseurl

--- mod_deliverystatus/tmpl/default.php
+++ mod_deliverystatus/tmpl/default.php
@@ -3,10 +3,10 @@
 defined( '_JEXEC' ) or die;
 ?>
 <div class="module">
-	<form method="get" action="/index.php">
+	<form method="get" action="<?php JUri::base() ?>/index.php">
 		<input name="option" value="com_deliverystatus" type="hidden" />
 		
-		<input name="parcel_number" class="replace" type="text" value="<?php echo JText::_('MOD_DELIVERYSTATUS_SUBMIT_BUTTON'); ?>" />
+		<input name="parcel_number" class="replace" type="text" placeholder="<?php echo JText::_('MOD_DELIVERYSTATUS_SUBMIT_BUTTON'); ?>" value="" />
 		
 		<input type="submit"/>
 	</form>
