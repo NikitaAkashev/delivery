b9867887f47d91708f059f1b6d76a6cbee87cc5a
Akashev Nikita <regspambox@yandex.ru>
2016-03-09 19:37:20 +0500

    Заменен шаблон

--- com_deliverystatus/com_deliverystatus/views/deliverystatus/tmpl/default.php
+++ com_deliverystatus/com_deliverystatus/views/deliverystatus/tmpl/default.php
@@ -3,53 +3,54 @@
 defined('_JEXEC') or die('Restricted access');
 ?>
 
+<div id="deliverystatus">
 <?php if (array_key_exists('parcel_number', $this->parcel) && $this->parcel['parcel_number']) { ?>
-	Номер накладной: <?php echo $this->parcel['parcel_number'] ?> <br />
+	<b>Номер накладной:</b> <?php echo $this->parcel['parcel_number'] ?> <br />
 <?php } ?>
 
 <?php if (array_key_exists('created', $this->parcel) && $this->parcel['created']) { ?>
-	Дата создания накладной: <?php $date = new DateTime($this->parcel['created']); echo $date->format('d.m.Y'); ?> <br />
+	<b>Дата создания накладной:</b> <?php $date = new DateTime($this->parcel['created']); echo $date->format('d.m.Y'); ?> <br />
 <?php } ?>
 
 <?php if (array_key_exists('sender', $this->parcel) && $this->parcel['sender']) { ?>
-	Отправитель: <?php echo $this->parcel['sender'] ?> <br />
+	<b>Отправитель:</b> <?php echo $this->parcel['sender'] ?> <br />
 <?php } ?>
 
 <?php if (array_key_exists('receiver', $this->parcel) && $this->parcel['receiver']) { ?>
-	Получатель: <?php echo $this->parcel['receiver'] ?> <br />
+	<b>Получатель:</b> <?php echo $this->parcel['receiver'] ?> <br />
 <?php } ?>
 
 <?php if (array_key_exists('payer', $this->parcel) && $this->parcel['payer']) { ?>
-	Плательщик: <?php echo $this->parcel['payer'] ?> <br />
+	<b>Плательщик:</b> <?php echo $this->parcel['payer'] ?> <br />
 <?php } ?>
 
 <?php if (array_key_exists('address_from', $this->parcel) && $this->parcel['address_from']) { ?>
-	Откуда: <?php echo $this->parcel['address_from'] ?> <br />
+	<b>Откуда:</b> <?php echo $this->parcel['address_from'] ?> <br />
 <?php } ?>
 
 <?php if (array_key_exists('address_to', $this->parcel) && $this->parcel['address_to']) { ?>
-	Куда: <?php echo $this->parcel['address_to'] ?> <br />
+	<b>Куда:</b> <?php echo $this->parcel['address_to'] ?> <br />
 <?php } ?>
 
 <?php if (array_key_exists('mem', $this->parcel) && $this->parcel['mem']) { ?>
-	Дополнительная информация: 
+	<b>Дополнительная информация:</b> 
 	<p><?php echo $this->parcel['mem'] ?></p>
 <?php } ?>
 
 <?php if (array_key_exists('places_amount', $this->parcel) && $this->parcel['places_amount']) { ?>
-	Количество мест: <?php echo $this->parcel['places_amount'] ?> <br />
+	<b>Количество мест:</b> <?php echo $this->parcel['places_amount'] ?> <br />
 <?php } ?>
 
 <?php if (array_key_exists('weight', $this->parcel) && $this->parcel['weight']) { ?>
-	Вес: <?php echo $this->parcel['weight'] ?> <br />
+	<b>Вес:</b> <?php echo $this->parcel['weight'] ?> <br />
 <?php } ?>
 
 <?php if (array_key_exists('volume', $this->parcel) && $this->parcel['volume']) { ?>
-	Объем: <?php echo $this->parcel['volume'] ?> <br />
+	<b>Объем:</b> <?php echo $this->parcel['volume'] ?> <br />
 <?php } ?>
 
 <?php if (count($this->statuses)) { ?>
-	Статус доставки: <br />
+	<b>Статус доставки:</b> <br />
 	<?php foreach($this->statuses as $status) :?>
 		<?php echo date("d.m.Y", strtotime($status->dt)) . ' ' . $status->name; ?><br />
 	<?php endforeach;?>
@@ -65,4 +66,5 @@ defined('_JEXEC') or die('Restricted access');
 		$href = '"'.JRoute::_('index.php?option=com_deliverystatus&Itemid='.$this->item_id.'&parcel_number='.$this->parcel_number.'&tmpl=component&print=1').'" '.$href;
 	}
 ?>
- 	<a href=<?php echo $href; ?> >Печать</a>
+ 	<a class="print" href=<?php echo $href; ?> >Печать</a>
+</div>
