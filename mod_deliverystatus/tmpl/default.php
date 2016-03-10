23bbcc38f3e58a33b9fed4f82ce67b7d6082cb2d
Akashev Nikita <regspambox@yandex.ru>
2016-03-10 22:32:11 +0500

    Изменен модуль

--- mod_deliverystatus/tmpl/default.php
+++ mod_deliverystatus/tmpl/default.php
@@ -11,10 +11,11 @@ defined( '_JEXEC' ) or die;
 ?>
  
 <div class="module">
+	<p><?php echo JText::_('MOD_DELIVERYSTATUS_DESC'); ?></p>
 	<form method="get" action="<?php echo JRoute::_('index.php?option=com_deliverystatus&Itemid='.$itemId); ?>">
 		
-		<input name="parcel_number" class="replace" type="text" placeholder="<?php echo JText::_('MOD_DELIVERYSTATUS_SUBMIT_BUTTON'); ?>" value="" />
+		<input name="parcel_number" class="replace" type="text" placeholder="<?php echo JText::_('MOD_DELIVERYSTATUS_PARCEL_NUMBER'); ?>" value="" />
 		
-		<input type="submit"/>
+		<input type="submit" value="<?php echo JText::_('MOD_DELIVERYSTATUS_SUBMIT_BUTTON'); ?>"/>
 	</form>
 </div>
