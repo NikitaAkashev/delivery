592c14b4dcaaac3a8b4a07024125f9de48fee1e1
Акашев Никита <regspreambox@yandex.ru>
2017-02-17 23:08:33 +0500

    Скрипт синхронизации

--- com_deliverystatus/admin/sync.php
+++ com_deliverystatus/admin/sync.php
@@ -1,61 +1,80 @@
 <?php
-
-function GetOrdersRemote()
+class Syncer
 {
-	$login = "";
-	$password = "";
-	$url = "";
-
-	$date = date("Y-m-d", time() - 60 * 60 * 24 * 5);
-
-	$secure = md5($date.'&'.$password);
-
-	$xml ='<?xml version="1.0" encoding="UTF8"?>
-	<StatusReport Date="'.$date.'" Account="'.$login.'" Secure="'.$secure.'" ShowHistory="1" >
-		<ChangePeriod DateFirst ="2013-07-16" DateLast ="2017-07-17" / >
-	</StatusReport>"';
-
-	// создание нового ресурса cURL
-	$ch = curl_init();
-	// установка URL и других необходимых параметров
-	curl_setopt($ch, CURLOPT_URL, $url.'?account='.$login.'&secure='.$secure.'&datefirst='.$date);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, "?xml_request=".urlencode($xml));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-	// загрузка страницы и выдача её браузеру
-	$out = curl_exec($ch);
-	// завершение сеанса и освобождение ресурсов
-	curl_close($ch);
-		
-	$order = simplexml_load_string ($out);
+	private $_cdek_login;
+	private $_cdek_password;
+	private $_cdek_url;
+	private $_cdek_days;
 	
-	$orders = array();
+	private $_mysqli;
+	private $_sql_prefix;
 	
-	foreach($order->Order as $ord )
+	function __construct($cdek_login, $cdek_password, $cdek_url, $cdek_days, 
+		$sql_host, $sql_user, $sql_password, $sql_db, $sql_prefix)
 	{
-		$row = array();
-		$attrs = $ord->attributes();
-		$status_attrs = $ord->Status->attributes();
+		$this->_cdek_login = $cdek_login;
+		$this->_cdek_password = $cdek_password;
+		$this->_cdek_url = $cdek_url;
+		$this->_cdek_days = $cdek_days;
 		
-		$row['outer_id'] = (string)$attrs['DispatchNumber'];	
-		$row['date'] = (string)$status_attrs['Date'];
-		$row['code'] = (string)$status_attrs['Code'];
+		$this->_mysqli = new mysqli($sql_host, $sql_user, $sql_password, $sql_db);
+		if ($mysqli->connect_error) {
+			die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+		}
 		
-		$orders[] = $row;
+		$this->_sql_prefix = $sql_prefix;
+	} 
+	
+	function __destruct()
+	{
+		$this->_mysqli->close();
 	}
 	
-	return $orders;
-}
+	private function RealQuery($query)
+	{
+		return str_replace("#__", $this->_sql_prefix, $query);
+	}
+	
+	private function GetOrdersRemote()
+	{
+		$date = date("Y-m-d", time() - 60 * 60 * 24 * $this->_cdek_days);
 
-function GetOrdersLocal()
-{
-	$table_prefix = 'fu28n_';
-	$mysqli = new mysqli('', '', '', '');
-	if ($mysqli->connect_error) {
-		die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+		$secure = md5($date.'&'.$this->_cdek_password);
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->_cdek_url.'?account='.$this->_cdek_login.'&secure='.$secure.'&datefirst='.$date);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+		
+		$out = curl_exec($ch);
+		
+		curl_close($ch);
+			
+		$order = simplexml_load_string ($out);
+		
+		$orders = array();
+		
+		if (!count($order->Order))
+			return;
+		
+		foreach($order->Order as $ord)
+		{
+			$row = array();
+			$attrs = $ord->attributes();
+			$status_attrs = $ord->Status->attributes();
+			
+			$row['outer_id'] = (string)$attrs['DispatchNumber'];	
+			$row['dt'] = (string)$status_attrs['Date'];
+			$row['code'] = (string)$status_attrs['Code'];
+			
+			$orders[] = $row;
+		}
+		
+		return $orders;
 	}
-	
-	$query = <<<TXT
+
+	private function GetOrdersLocal()
+	{
+		$query = <<<TXT
 select 
 	p.parcel, 
 	p.outer_id,
@@ -71,19 +90,73 @@ left join (
 left join #__delivery_parcel2parcel_status ps on ps.parcel = p.parcel and ps.dt = pdt.dt
 left join #__delivery_parcel_status s on s.parcel_status = ps.parcel_status
 where
-    p.outer_id <> ''
-    and p.outer_id is not null
+	p.outer_id <> ''
+	and p.outer_id is not null
+	and s.code <> '4' -- статус не "Вручен"
 TXT;
-	
-	$query = str_replace("#__", $table_prefix, $query);
-	
-	$result = $mysqli->query($query);
-	
-	$orders = $result->fetch_all(MYSQLI_ASSOC);
-	
-	return $orders;
+		
+		$query = $this->RealQuery($query);
+		
+		$result = $this->_mysqli->query($query);
+		
+		$orders = $result->fetch_all(MYSQLI_ASSOC);
+		
+		return $orders;
+	}
+
+	private function SetStatus($data)
+	{
+		$template = <<<TXT
+insert into #__delivery_parcel2parcel_status(parcel, parcel_status, dt)
+select '%s', parcel_status, '%s' from #__delivery_parcel_status where code = '%s';
+
+TXT;
+		$query = '';
+		
+		foreach($data as $r)
+		{
+			$query .= sprintf($template, 
+				$this->_mysqli->real_escape_string($r['parcel']), 
+				$this->_mysqli->real_escape_string($r['dt']), 
+				$this->_mysqli->real_escape_string($r['code']));
+		}
+		$query = $this->RealQuery($query);
+		
+		if($this->_mysqli->multi_query($query) !== TRUE)
+			echo $this->_mysqli->error;
+	}
+
+	function Sync()
+	{
+		$remote = $this->GetOrdersRemote();
+		if (!count($remote))
+			return;
+		
+		$local = $this->GetOrdersLocal();
+		if (!count($local))
+			return;
+		
+		foreach($local as $loc)
+		{
+			foreach($remote as $rem)
+			{
+				if($rem['outer_id'] == $loc['outer_id'] && $rem['code'] != $loc['code'])
+				{
+					$new_data[] = array('parcel' => $loc['parcel'], 'code' => $rem['code'], 'dt' => $rem['dt']);
+				}
+			}
+		}
+		
+		if (!count($new_data))
+			return;
+		
+		$this->SetStatus($new_data);		
+	}
 }
 
-print_r(GetOrdersRemote());
-print_r(GetOrdersLocal());
+$worker = new Syncer("cdeklogin", "cdekpass", "cdekurl", 3,
+	'sqlhost', 'sqluser', 'sqlpass', 'sqldb', 'sqlprefix');
+
+$worker->Sync();
+
 ?>
